121cd6e41bc8d6b48066cde76b34192f550876ab
kmlefran <kgagnon@lakeheadu.ca>
2023-07-10 16:15:48 -0400

    add long_description

--- setup.py
+++ setup.py
@@ -1,10 +1,19 @@
 import setuptools
-setuptools.setup(name='subproptools',
-version='0.0.2',
-description='Extract group properties from .sum file and rotate substituent geometries',
-url='https://github.com/kmlefran/subproptools',
-author='Kevin M. Lefrancois-Gagnon',
-install_requires=['numpy','pandas'],
-author_email='kgagnon@lakeheadu.ca',
-packages=setuptools.find_packages(),
-zip_safe=False)
+
+setuptools.setup(
+    name='subproptools',
+    version='0.0.2',
+    description='Extract group properties from .sum file and rotate substituent geometries',
+    long_description=open('README.md').read(),
+    url='https://github.com/kmlefran/subproptools',
+    author='Kevin M. Lefrancois-Gagnon',
+    install_requires=['numpy','pandas'],
+    author_email='kgagnon@lakeheadu.ca',
+    packages=setuptools.find_packages(),
+    classifiers = [
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.11',
+    ],
+    zip_safe=False,
+)
