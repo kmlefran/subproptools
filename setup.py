c6a8358772d7f24aee04943fb3c8111f8401c325
kmlefran <kgagnon@lakeheadu.ca>
2023-09-01 14:16:57 -0400

    Increment version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name='subproptools',
-    version='0.1.15',
+    version='0.1.16',
     description='Extract group properties from .sum file and rotate substituent geometries',
     long_description=open('README.md').read(),
     url='https://github.com/kmlefran/subproptools',
